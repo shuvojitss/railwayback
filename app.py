81b37c32eaa03c71ebeac5cc19f9c9011d468f81
Shuvojit Samanta <142198651+shuvojitss@users.noreply.github.com>
2026-04-22 23:35:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -544,4 +544,4 @@ async def api_github_callback(request: Request):
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run("app:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("app:app", host="127.0.0.1", port=8080, reload=True)
